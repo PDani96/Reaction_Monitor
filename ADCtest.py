7c6f31307d5819205ed4ba31bf3e036df3b809b3
PDani96 <preston.danielson@gmail.com>
2022-04-01 03:54:27 -0500

    Updated ADC test to read all 7 pressure sensors.

--- ADCtest.py
+++ ADCtest.py
@@ -5,7 +5,13 @@ import time
 
 #Define Variables
 delay = 0.5
-pad_channel = 0
+pad1_channel = 0
+pad2_channel = 1
+pad3_channel = 2
+pad4_channel = 3
+pad5_channel = 4
+pad6_channel = 5
+pad7_channel = 6
 
 #Create SPI
 spi = spidev.SpiDev()
@@ -22,9 +28,21 @@ def readadc(adcnum):
 
 try:
     while True:
-        pad_value = readadc(pad_channel)
+        pad1_value = readadc(pad1_channel)
+        pad2_value = readadc(pad2_channel)
+        pad3_value = readadc(pad3_channel)
+        pad4_value = readadc(pad4_channel)
+        pad5_value = readadc(pad5_channel)
+        pad6_value = readadc(pad6_channel)
+        pad7_value = readadc(pad7_channel)
         print("---------------------------------------")
-        print("Pressure Pad Value: %d" % pad_value)
+        print("Pressure Pad 1 Value: %d" % pad1_value)
+        print("Pressure Pad 2 Value: %d" % pad2_value)
+        print("Pressure Pad 3 Value: %d" % pad3_value)
+        print("Pressure Pad 4 Value: %d" % pad4_value)
+        print("Pressure Pad 5 Value: %d" % pad5_value)
+        print("Pressure Pad 6 Value: %d" % pad6_value)
+        print("Pressure Pad 7 Value: %d" % pad7_value)
         time.sleep(delay)
 except KeyboardInterrupt:
     pass
